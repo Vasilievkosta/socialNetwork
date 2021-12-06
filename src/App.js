ecd40374a302fff5a6b7f15dad68d512596e209b
Vasilievkosta <vasilievkosta@gmail.com>
2021-12-07 00:10:37 +0200

    add login L61

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header/Header';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
@@ -17,7 +17,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<div className='app-wrapper'>
-				<Header />
+				<HeaderContainer />
 				<Navbar />
 				<div className='app-content'>
 					{/* <Route path='/profile' component={Profile} /> можно так, через component*/}
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,12 +1,14 @@
-import { combineReducers, createStore } from "redux";
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import usersReducer from "./users-reducer";
+import { combineReducers, createStore } from 'redux';
+import profileReducer from './profile-reducer';
+import dialogsReducer from './dialogs-reducer';
+import usersReducer from './users-reducer';
+import authReducer from './auth-reducer';
 
 let reducers = combineReducers({
 	profilePage: profileReducer,
 	messagesPage: dialogsReducer,
-	usersPage: usersReducer
+	usersPage: usersReducer,
+	auth: authReducer
 });
 
 let store = createStore(reducers);
